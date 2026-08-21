57c7dff9edaa05ae4fe2d6d7e9de3fe123baf7de
hojinS-code <ghwls175871@soluwins.com>
2026-08-21 15:11:32 +0900

    Read DATABSE_URL from enviroment variable

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql+psycopg2://postgres:1234@localhost:5432/bookstore_db"
+DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql+psycopg2://postgres:1234@localhost:5432/bookstore_db")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
