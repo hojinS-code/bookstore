dacccba8c6e2b39cf5f19ac93e3cbf6e2aee2256
hojinS-code <ghwls175871@soluwins.com>
2026-08-24 17:50:57 +0900

    Clean up temporary scripts

--- backend/add_column.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import text
-from database import engine
-
-with engine.connect() as conn:
-    conn.execute(text("ALTER TABLE users ADD COLUMN is_admin BOOLEAN DEFAULT FALSE"))
-    conn.commit()
-    
-print("완료!")
--- backend/list_users.py
+++
@@ -1,8 +0,0 @@
-from database import SessionLocal
-from models.user import User
-
-db = SessionLocal()
-users = db.query(User).all()
-for user in users:
-    print(user.username)
-db.close()
